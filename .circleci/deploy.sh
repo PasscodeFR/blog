9d039d4227717e0251351cd3d06266daeec25467
DecodeGroup <45751255+DecodeGroup@users.noreply.github.com>
2018-12-14 01:14:06 +0800

    Add deploy script

---
+++ .circleci/deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+git config --global push.default simple
+git config --global user.email $(git --no-pager show -s --format='%ae' HEAD)
+git config --global user.name $CIRCLE_USERNAME
+
+git clone -q --branch=master $REPOSITORY_URL deployment
+
+cd $DEPLOY_DIR
+rsync -arv --delete ../public/* .
+
+git add -f .
+git commit -m "Deploy #$CIRCLE_BUILD_NUM: $(date "+%Y-%m-%d %H:%M:%S")" || true
+git push -f
