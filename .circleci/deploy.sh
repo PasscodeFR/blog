c5ddaf5217a760e6244018ecfcde0038cc70b098
pmmmwh <pmmmwh@gmail.com>
2018-12-14 01:21:53 +0800

    Add repo url in deploy script

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+REPOSITORY_URL=https://github.com/PasscodeFR/PasscodeFR.github.io.git
+
 git config --global push.default simple
 git config --global user.email $(git --no-pager show -s --format='%ae' HEAD)
 git config --global user.name $CIRCLE_USERNAME
