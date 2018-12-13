5b81d3f682bb128cc8612121f11b741a5cdaed6d
pmmmwh <pmmmwh@gmail.com>
2018-12-14 01:34:12 +0800

    Update deploy.sh

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -2,16 +2,17 @@
 
 set -e
 
+DEPLOY_DIR=deployment
 REPOSITORY_URL=https://github.com/PasscodeFR/PasscodeFR.github.io.git
 
 git config --global push.default simple
 git config --global user.email $(git --no-pager show -s --format='%ae' HEAD)
 git config --global user.name $CIRCLE_USERNAME
 
-git clone -q --branch=master $REPOSITORY_URL deployment
+git clone -q --branch=master $REPOSITORY_URL $DEPLOY_DIR
 
 cd $DEPLOY_DIR
-rsync -arv --delete ./public/* .
+rsync -arv --delete ../public/* .
 
 git add -f .
 git commit -m "Deploy #$CIRCLE_BUILD_NUM: $(date "+%Y-%m-%d %H:%M:%S")" || true
